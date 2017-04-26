c908b8a4eb364203eb82bbb90e9047fe49fddc22
Chensong Zhang <zhangchensong@gmail.com>
2017-04-26 14:10:47 +0800

    Update examples

--- examples/TensorFlow/simple.py
+++ examples/TensorFlow/simple.py
@@ -1,22 +1,32 @@
 import tensorflow as tf
 
+# Define a computational graph for TensorFlow
 graph = tf.Graph()
 
+# Define operation nodes in the graph
 with graph.as_default():
+
     with tf.name_scope('input'):
+        # Define two constant symbols (two nodes)
         a = tf.constant(1, name='a')
         b = tf.constant(2, name='b')
 
     with tf.name_scope('output'):
+        # Define an operation node: c = a * b
         c = tf.multiply(a, b)
         tf.summary.histogram('c', c)
     
+    # Merge all summaries and write to logdir
+    logdir = '/tmp/tensorboard'
     merged = tf.summary.merge_all()
-    writer = tf.summary.FileWriter('./board', graph)
+    writer = tf.summary.FileWriter(logdir, graph)
 
 with tf.Session(graph=graph) as sess:
+    # Compute and output the result (c=a*b)
     result = sess.run(c)
     print result
+    
+    # Write TensorBoard data to logdir
     writer.add_summary(merged.eval())
 
 writer.close()
--- examples/TensorFlow/softmax.py
+++ examples/TensorFlow/softmax.py
@@ -1,48 +1,50 @@
-#-*-coding:utf-8-*-
-
 import tensorflow as tf
 
+# Import training and test data from MNIST
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 graph = tf.Graph()
 
-# 下面大量使用 tf.name_scope() 可以使在浏览器中得到 computational graph 更形象更容易阅读
 with graph.as_default():
+
+    # Nodes can be grouped into visual blocks for TensorBoard
     with tf.name_scope('input_features'):
         x = tf.placeholder(tf.float32, shape=[None, 784], name='input_x')
+
     with tf.name_scope('input_labels'):
         y_ = tf.placeholder(tf.float32, shape=[None, 10], name='labels')
 
-    with tf.name_scope('input_images'):
-        image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
-        tf.summary.image('input', image_shaped_input, 20)
-
     with tf.name_scope('parameters'):
         W = tf.Variable(tf.zeros([784, 10]), name='weights')
-        tf.summary.histogram('WEIGHTS', W)
         b = tf.Variable(tf.zeros([10]), name='biases')
+        tf.summary.histogram('WEIGHTS', W)
         tf.summary.histogram('BIASES', b)
     
     with tf.name_scope('use_softmax'):
         y = tf.nn.softmax(tf.matmul(x, W) + b)
 
     with tf.name_scope('train'):
+        # Compute the cross entropy of real label y_ and prediction label y
         cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+        # Create a gradient-descent optimizer with learning rate = 0.01
         train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
     with tf.name_scope('test'):
         correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        # Track accuracy over time for TensorBoard
         tf.summary.scalar('Accuracy', accuracy)
 
-    #merged = tf.summary.merge([accuracy_summary])
-    merged = tf.summary.merge_all()
-    writer = tf.summary.FileWriter('./board', graph)
+    logdir = '/tmp/tensorboard'  # temporary path for storing TB summaries
+    merged = tf.summary.merge_all()  # Merge all the summaries
+    writer = tf.summary.FileWriter(logdir, graph)  # Write summaries
 
 with tf.Session(graph=graph) as sess:
+    # Initialize all variables
     tf.global_variables_initializer().run()
-    for step in range(1000):
+    
+    for step in range(1,501):
         if (step%10) == 0:
             feed = {x: mnist.test.images, y_: mnist.test.labels}
             _, acc = sess.run([merged, accuracy], feed_dict=feed)
@@ -51,4 +53,9 @@ with tf.Session(graph=graph) as sess:
             batch_x, batch_y = mnist.train.next_batch(100)
             sess.run(train_step, feed_dict={x: batch_x, y_: batch_y})
             writer.add_summary(merged.eval(feed_dict={x: batch_x, y_: batch_y}), global_step=step)
+
     writer.close()
+
+print("Run the command line to start TensorBoard:\n" \
+      "(TensorFlow) $ tensorboard --logdir=/tmp/tensorboard" \
+      "\nThen open http://0.0.0.0:6006/ into your web browser")
